e123561f884f2fcd652caed003d5c089db55fe1e
TheAkseon <abqwerty390@gmail.com>
2023-06-24 15:07:13 +0300

    edit shadow settings

--- Assets/Application/Scripts/Obstacles/RotateObstacle.cs
+++ Assets/Application/Scripts/Obstacles/RotateObstacle.cs
@@ -2,10 +2,4 @@ using UnityEngine;
 
 public class RotateObstacle : Obstacle
 {
-    [SerializeField] private float _speed;
-
-    private void Update()
-    {
-        transform.Rotate(0, _speed * Time.deltaTime, 0);
-    }
 }
